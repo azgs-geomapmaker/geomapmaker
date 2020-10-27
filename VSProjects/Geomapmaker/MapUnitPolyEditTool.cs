f2dbcd236de0802740c56abec88a38cb487d729a
Noisy Flowers <noisyflowers@douglasmeredith.net>
2020-10-27 15:28:58 -0700

    Set handle to self in GeomapmakerModule. Add Clear method. Set pane heading in view model. Select feature in map to highlight it. Check for list content before accessing.

--- VSProjects/Geomapmaker/MapUnitPolyEditTool.cs
+++ VSProjects/Geomapmaker/MapUnitPolyEditTool.cs
@@ -20,13 +20,19 @@ using Geomapmaker.Models;
 namespace Geomapmaker {
 	internal class MapUnitPolyEditTool : MapTool {
 		public MapUnitPolyEditTool() {
+			GeomapmakerModule.EditMapUnitPolyTool = this;
 			IsSketchTool = true;
 			SketchType = SketchGeometryType.Point;
 			SketchOutputMode = SketchOutputMode.Map;
 		}
 
+		public void Clear() {
+			ClearSketchAsync();
+		}
+
 		protected override Task OnToolActivateAsync(bool active) {
 			AddEditMapUnitPolysDockPaneViewModel.Show();
+			GeomapmakerModule.MapUnitPolysVM.Heading = "Edit Map Unit Poly";
 			return base.OnToolActivateAsync(active);
 		}
 		protected override Task OnToolDeactivateAsync(bool hasMapViewChanged) {
@@ -38,45 +44,49 @@ namespace Geomapmaker {
 			var mv = MapView.Active;
 			var identifyResult = await QueuedTask.Run(() => {
 
+				mv.SelectFeatures(geometry);
+
 				// Get the features that intersect the sketch geometry.
 				var features = mv.GetFeatures(geometry);
 				//Only interested in MapUnitPolys
 				var mapUnitFeatures = features.Where(x => x.Key.Name == "MapUnitPolys");
-				if (mapUnitFeatures != null) {
+				if (mapUnitFeatures.Count() > 0) {
 					//Get map unit objectid
 					//TODO: I am only pulling the first from the list. Might need to present some sort of selector to the user. 
-					var mapUnitPolyID = mapUnitFeatures.First().Value.First();
+					if (mapUnitFeatures.First().Value.Count() > 0) {
+						var mapUnitPolyID = mapUnitFeatures.First().Value.First();
 
-					//Using the objectid, get the map unit record from the database
-					using (Geodatabase geodatabase = new Geodatabase(DataHelper.connectionProperties)) {
-						using (Table mapUnitPolysTable = geodatabase.OpenDataset<Table>("MapUnitPolys")) {
-							QueryFilter queryFilter = new QueryFilter {
-								WhereClause = "objectid = " + mapUnitPolyID
-							};
-							var mapUnitPoly = new MapUnitPoly();
-							using (RowCursor rowCursor = mapUnitPolysTable.Search(queryFilter, false)) {
-								if (rowCursor.MoveNext()) {
-									using (Row row = rowCursor.Current) {
-										mapUnitPoly.ID = Int32.Parse(Convert.ToString(row["objectid"]));
-										mapUnitPoly.MapUnit = DataHelper.MapUnits.Where(x => x.MU == Convert.ToString(row["mapunit"])).First();
-										mapUnitPoly.Label = Convert.ToString(row["label"]);
-										mapUnitPoly.Notes = Convert.ToString(row["notes"]);
-										mapUnitPoly.Symbol = Convert.ToString(row["symbol"]);
-										mapUnitPoly.IdentityConfidence = Convert.ToString(row["identityconfidence"]);
-										mapUnitPoly.DataSource = Convert.ToString(row["datasourceid"]);
-										mapUnitPoly.Shape = (Geometry)row["shape"]; //TODO: no idea
+						//Using the objectid, get the map unit record from the database
+						using (Geodatabase geodatabase = new Geodatabase(DataHelper.connectionProperties)) {
+							using (Table mapUnitPolysTable = geodatabase.OpenDataset<Table>("MapUnitPolys")) {
+								QueryFilter queryFilter = new QueryFilter {
+									WhereClause = "objectid = " + mapUnitPolyID
+								};
+								var mapUnitPoly = new MapUnitPoly();
+								using (RowCursor rowCursor = mapUnitPolysTable.Search(queryFilter, false)) {
+									if (rowCursor.MoveNext()) {
+										using (Row row = rowCursor.Current) {
+											mapUnitPoly.ID = Int32.Parse(Convert.ToString(row["objectid"]));
+											mapUnitPoly.MapUnit = DataHelper.MapUnits.Where(x => x.MU == Convert.ToString(row["mapunit"])).First();
+											mapUnitPoly.Label = Convert.ToString(row["label"]);
+											mapUnitPoly.Notes = Convert.ToString(row["notes"]);
+											mapUnitPoly.Symbol = Convert.ToString(row["symbol"]);
+											mapUnitPoly.IdentityConfidence = Convert.ToString(row["identityconfidence"]);
+											mapUnitPoly.DataSource = Convert.ToString(row["datasourceid"]);
+											mapUnitPoly.Shape = (Geometry)row["shape"]; //TODO: no idea
+										}
 									}
+
 								}
 
+								// Use the map unit to populate the view model for the form
+								GeomapmakerModule.MapUnitPolysVM.SelectedMapUnitPoly = mapUnitPoly;
+								GeomapmakerModule.MapUnitPolysVM.SelectedMapUnit = mapUnitPoly.MapUnit;
+								//TODO: The line below forces a refresh on the object the UI is bound to for shape. But other than that,
+								//it's redundant. There should be a better way to handle this.
+								GeomapmakerModule.MapUnitPolysVM.Shape = GeomapmakerModule.MapUnitPolysVM.SelectedMapUnitPoly.Shape;
+
 							}
-
-							// Use the map unit to populate the view model for the form
-							GeomapmakerModule.MapUnitPolysVM.SelectedMapUnitPoly = mapUnitPoly;
-							GeomapmakerModule.MapUnitPolysVM.SelectedMapUnit = mapUnitPoly.MapUnit;
-							//TODO: The line below forces a refresh on the object the UI is bound to for shape. But other than that,
-							//it's redundant. There should be a better way to handle this.
-							GeomapmakerModule.MapUnitPolysVM.Shape = GeomapmakerModule.MapUnitPolysVM.SelectedMapUnitPoly.Shape;
-
 						}
 					}
 				}
