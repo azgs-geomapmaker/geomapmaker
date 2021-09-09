7f964eb14108a685a62484ca3f2bd457e5b1b515
Michael Camp <camp@email.arizona.edu>
2021-09-09 11:00:20 -0700

    Made NewCommandHandler (Reset CF dockpane) a synchronous method. Added a new row to xaml.

--- VSProjects/Geomapmaker/Views/AddEditContactsAndFaultsDockPane.xaml.cs
+++ VSProjects/Geomapmaker/Views/AddEditContactsAndFaultsDockPane.xaml.cs
@@ -14,41 +14,49 @@ using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 
-namespace Geomapmaker {
-	/// <summary>
-	/// Interaction logic for AddEditContactsAndFaultsDockPaneView.xaml
-	/// </summary>
-	public partial class AddEditContactsAndFaultsDockPaneView : UserControl {
-		public AddEditContactsAndFaultsDockPaneView() {
-			InitializeComponent();
-		}
+namespace Geomapmaker
+{
+    /// <summary>
+    /// Interaction logic for AddEditContactsAndFaultsDockPaneView.xaml
+    /// </summary>
+    public partial class AddEditContactsAndFaultsDockPaneView : UserControl
+    {
+        public AddEditContactsAndFaultsDockPaneView()
+        {
+            InitializeComponent();
+        }
 
-		private async void NewCommandHandler(object sender, ExecutedRoutedEventArgs e) {
-			// Calls a method to close the file and release resources.
-			var vm = (AddEditContactsAndFaultsDockPaneViewModel)this.DataContext;
-			vm.Reset();
-		}
+        private void NewCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            // Calls a method to close the file and release resources.
+            var vm = (AddEditContactsAndFaultsDockPaneViewModel)this.DataContext;
+            vm.Reset();
+        }
 
-		private async void SaveCommandHandler(object sender, ExecutedRoutedEventArgs e) {
-			// Calls a method to close the file and release resources.
-			var vm = (AddEditContactsAndFaultsDockPaneViewModel)this.DataContext;
-			await vm.saveCF();
-			vm.Reset();
-			//ContactsAndFaultsComboBox.SelectedIndex = -1;
-		}
+        private async void SaveCommandHandler(object sender, ExecutedRoutedEventArgs e)
+        {
+            // Calls a method to close the file and release resources.
+            var vm = (AddEditContactsAndFaultsDockPaneViewModel)this.DataContext;
+            await vm.saveCF();
+            vm.Reset();
+            //ContactsAndFaultsComboBox.SelectedIndex = -1;
+        }
 
-		private void SaveCanExecuteHandler(object sender, CanExecuteRoutedEventArgs e) {
-			// Call a method to determine if there is a file open.
-			// If there is a file open, then set CanExecute to true.
-			var vm = (AddEditContactsAndFaultsDockPaneViewModel)this.DataContext;
-			if (vm != null && vm.IsValid) {
-				e.CanExecute = true;
-			}
-			// if there is not a file open, then set CanExecute to false.
-			else {
-				e.CanExecute = false;
-			}
-		}
+        private void SaveCanExecuteHandler(object sender, CanExecuteRoutedEventArgs e)
+        {
+            // Call a method to determine if there is a file open.
+            // If there is a file open, then set CanExecute to true.
+            var vm = (AddEditContactsAndFaultsDockPaneViewModel)this.DataContext;
+            if (vm != null && vm.IsValid)
+            {
+                e.CanExecute = true;
+            }
+            // if there is not a file open, then set CanExecute to false.
+            else
+            {
+                e.CanExecute = false;
+            }
+        }
 
-	}
+    }
 }
