062c4ff8b590bab7e03b0ada2d1be85409ba7934
Michael Camp <camp@email.arizona.edu>
2021-09-16 15:54:38 -0700

    Added new ribbon button and dockpane for Add/Edit Headings

---
+++ VSProjects/Geomapmaker/ViewModels/HeadingsViewModel.cs
@@ -0,0 +1,48 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace Geomapmaker
+{
+    internal class HeadingsViewModel : DockPane
+    {
+        private const string _dockPaneID = "Geomapmaker_Headings";
+
+        protected HeadingsViewModel() { }
+
+        /// <summary>
+        /// Show the DockPane.
+        /// </summary>
+        internal static void Show()
+        {
+            DockPane pane = FrameworkApplication.DockPaneManager.Find(_dockPaneID);
+            if (pane == null)
+                return;
+
+            pane.Activate();
+        }
+
+        /// <summary>
+        /// Text shown near the top of the DockPane.
+        /// </summary>
+        private string _heading = "Add/Edit Headings";
+        public string Heading
+        {
+            get { return _heading; }
+            set
+            {
+                SetProperty(ref _heading, value, () => Heading);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Button implementation to show the DockPane.
+    /// </summary>
+    internal class Headings_ShowButton : Button
+    {
+        protected override void OnClick()
+        {
+            HeadingsViewModel.Show();
+        }
+    }
+}
