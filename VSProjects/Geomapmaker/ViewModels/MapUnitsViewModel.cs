35ae52256641b91ed93967031cb2932e3d438e8b
Michael Camp <camp@email.arizona.edu>
2021-09-29 15:28:40 -0700

    Added a new dockpane for map units

---
+++ VSProjects/Geomapmaker/ViewModels/MapUnitsViewModel.cs
@@ -0,0 +1,65 @@
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Core.Data;
+using ArcGIS.Core.Geometry;
+using ArcGIS.Desktop.Catalog;
+using ArcGIS.Desktop.Core;
+using ArcGIS.Desktop.Editing;
+using ArcGIS.Desktop.Extensions;
+using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Dialogs;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Layouts;
+using ArcGIS.Desktop.Mapping;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Geomapmaker
+{
+    internal class MapUnitsViewModel : DockPane
+    {
+        private const string _dockPaneID = "Geomapmaker_MapUnits";
+
+        protected MapUnitsViewModel() { }
+
+        /// <summary>
+        /// Show the DockPane.
+        /// </summary>
+        internal static void Show()
+        {
+            DockPane pane = FrameworkApplication.DockPaneManager.Find(_dockPaneID);
+            if (pane == null)
+                return;
+
+            pane.Activate();
+        }
+
+        /// <summary>
+        /// Text shown near the top of the DockPane.
+        /// </summary>
+        private string _heading = "Map Units";
+        public string Heading
+        {
+            get { return _heading; }
+            set
+            {
+                SetProperty(ref _heading, value, () => Heading);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Button implementation to show the DockPane.
+    /// </summary>
+    internal class MapUnits_ShowButton : Button
+    {
+        protected override void OnClick()
+        {
+            MapUnitsViewModel.Show();
+        }
+    }
+}
---
+++ VSProjects/Geomapmaker/Views/MapUnits.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+
+namespace Geomapmaker
+{
+    /// <summary>
+    /// Interaction logic for MapUnitsView.xaml
+    /// </summary>
+    public partial class MapUnitsView : UserControl
+    {
+        public MapUnitsView()
+        {
+            InitializeComponent();
+        }
+    }
+}
