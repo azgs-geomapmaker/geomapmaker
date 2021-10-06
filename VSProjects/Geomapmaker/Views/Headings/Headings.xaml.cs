01d5acccc6207df48530594607db2eed075a874c
Michael Camp <camp@email.arizona.edu>
2021-10-06 11:37:51 -0700

    Created a seperate xaml view for each of the tabs in the headings dockpane

---
+++ VSProjects/Geomapmaker/Views/Headings/Headings.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Geomapmaker.Views.Headings
+{
+    /// <summary>
+    /// Interaction logic for Headings.xaml
+    /// </summary>
+    public partial class Headings : UserControl
+    {
+        public Headings()
+        {
+            InitializeComponent();
+        }
+    }
+}
