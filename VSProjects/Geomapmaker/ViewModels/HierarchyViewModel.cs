8901d9fd703cfc83f43f0e98349a3d3e22e927e4
Michael Camp <camp@email.arizona.edu>
2021-10-14 09:07:49 -0700

    Created hierarchy dockpane

---
+++ VSProjects/Geomapmaker/ViewModels/HierarchyViewModel.cs
@@ -0,0 +1,45 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace Geomapmaker.ViewModels
+{
+    internal class HierarchyViewModel : DockPane
+    {
+        private const string _dockPaneID = "Geomapmaker_Hierarchy";
+
+        protected HierarchyViewModel() { }
+
+        /// <summary>
+        /// Show the DockPane.
+        /// </summary>
+        internal static void Show()
+        {
+            DockPane pane = FrameworkApplication.DockPaneManager.Find(_dockPaneID);
+            pane?.Activate();
+        }
+
+        /// <summary>
+        /// Text shown near the top of the DockPane.
+        /// </summary>
+        private string _heading = "Testing123";
+        public string Heading
+        {
+            get { return _heading; }
+            set
+            {
+                SetProperty(ref _heading, value, () => Heading);
+            }
+        }
+    }
+
+    /// <summary>
+    /// Button implementation to show the DockPane.
+    /// </summary>
+    internal class Hierarchy_ShowButton : Button
+    {
+        protected override void OnClick()
+        {
+            HierarchyViewModel.Show();
+        }
+    }
+}
---
+++ VSProjects/Geomapmaker/Views/Hierarchy.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Geomapmaker.Views
+{
+    /// <summary>
+    /// Interaction logic for HierarchyView.xaml
+    /// </summary>
+    public partial class HierarchyView : UserControl
+    {
+        public HierarchyView()
+        {
+            InitializeComponent();
+        }
+    }
+}
