446ec37242ed882b08be3f20246227c73bd56a67
Michael Camp <camp@email.arizona.edu>
2021-11-23 09:08:38 -0700

    Added new window to ribbon for data sources

---
+++ VSProjects/Geomapmaker/Views/DataSources.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace Geomapmaker.Views
+{
+    /// <summary>
+    /// Interaction logic for DataSources.xaml
+    /// </summary>
+    public partial class DataSources : ArcGIS.Desktop.Framework.Controls.ProWindow
+    {
+        public DataSources()
+        {
+            InitializeComponent();
+        }
+    }
+}
