12eca3d6d8c2dfcfd3ced95fa6c269dd9bbd630d
Michael Camp <camp@email.arizona.edu>
2022-01-27 11:54:53 -0700

    Populate the default values when an existing CF template is selected to edit

--- VSProjects/Geomapmaker/ViewModels/ContactsFaults/EditContactFaultVM.cs
+++ VSProjects/Geomapmaker/ViewModels/ContactsFaults/EditContactFaultVM.cs
@@ -1,9 +1,14 @@
-﻿using ArcGIS.Desktop.Editing.Templates;
+﻿using ArcGIS.Core.CIM;
+using ArcGIS.Desktop.Editing.Attributes;
+using ArcGIS.Desktop.Editing.Templates;
 using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using Geomapmaker.Models;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Linq;
 using System.Windows.Input;
 
 namespace Geomapmaker.ViewModels.ContactsFaults
@@ -13,6 +18,19 @@ namespace Geomapmaker.ViewModels.ContactsFaults
         public ContactsFaultsViewModel ParentVM { get; set; }
 
         public ICommand CommandUpdate => new RelayCommand(() => UpdateTemplate(), () => IsValid());
+
+        public EditContactFaultVM(ContactsFaultsViewModel parentVM)
+        {
+            ParentVM = parentVM;
+
+            // Initialize required fields
+            Type = "";
+            Label = "";
+            Symbol = null;
+            IdentityConfidence = "";
+            ExistenceConfidence = "";
+            LocationConfidenceMeters = "";
+        }
 
         private void UpdateTemplate()
         {
@@ -26,11 +44,6 @@ namespace Geomapmaker.ViewModels.ContactsFaults
             return true;
         }
 
-        public EditContactFaultVM(ContactsFaultsViewModel parentVM)
-        {
-            ParentVM = parentVM;
-        }
-
         private EditingTemplate _selected;
         public EditingTemplate Selected
         {
@@ -38,11 +51,187 @@ namespace Geomapmaker.ViewModels.ContactsFaults
             set
             {
                 SetProperty(ref _selected, value, () => Selected);
+                SetEditValues(Selected);
                 NotifyPropertyChanged("Visibility");
             }
         }
 
+        private async void SetEditValues(EditingTemplate template)
+        {
+            await QueuedTask.Run(() =>
+            {
+                CIMFeatureTemplate templateDef = template.GetDefinition() as CIMFeatureTemplate;
+
+                // Clear out filters
+                KeyFilter = "";
+                DescriptionFilter = "";
+
+                // Find the symbol
+                Symbol = ParentVM.SymbolOptions.FirstOrDefault(a => a.Key == templateDef.DefaultValues["symbol"].ToString());
+
+                // Set values from template
+                Type = templateDef.DefaultValues["type"].ToString();
+                Label = templateDef.DefaultValues["label"].ToString();
+                IdentityConfidence = templateDef.DefaultValues["identityconfidence"].ToString();
+                ExistenceConfidence = templateDef.DefaultValues["existenceconfidence"].ToString();
+                LocationConfidenceMeters = templateDef.DefaultValues["locationconfidencemeters"].ToString();
+                IsConcealed = templateDef.DefaultValues["isconcealed"].ToString() == "Y";
+                DataSource = templateDef.DefaultValues["datasourceid"].ToString();
+
+                // Notes is an optional field
+                if (templateDef.DefaultValues.ContainsKey("notes"))
+                {
+                    Notes = templateDef.DefaultValues["notes"].ToString();
+                }
+
+            });
+        }
+
         public string Visibility => Selected == null ? "Hidden" : "Visible";
+
+        private string _type;
+        public string Type
+        {
+            get => _type;
+            set
+            {
+                SetProperty(ref _type, value, () => Type);
+                ValidateRequiredString(Type, "Type");
+            }
+        }
+
+        private string _label;
+        public string Label
+        {
+            get => _label;
+            set
+            {
+                SetProperty(ref _label, value, () => Label);
+                ValidateRequiredString(Label, "Label");
+            }
+        }
+
+        private string _keyFilter;
+        public string KeyFilter
+        {
+            get => _keyFilter;
+            set
+            {
+                SetProperty(ref _keyFilter, value, () => KeyFilter);
+                FilterSymbols(KeyFilter, DescriptionFilter);
+            }
+        }
+
+        private string _descriptionFilter;
+        public string DescriptionFilter
+        {
+            get => _descriptionFilter;
+            set
+            {
+                SetProperty(ref _descriptionFilter, value, () => DescriptionFilter);
+                FilterSymbols(KeyFilter, DescriptionFilter);
+            }
+        }
+
+        // Filter the Symbol options by key and description
+        private void FilterSymbols(string keyFilter, string DescriptionFilter)
+        {
+            // Start with all the symbols from the parent vm
+            List<CFSymbol> filteredSymbols = ParentVM.SymbolOptions;
+
+            // Filter by key
+            if (!string.IsNullOrEmpty(keyFilter))
+            {
+                filteredSymbols = filteredSymbols.Where(a => a.Key.StartsWith(keyFilter)).ToList();
+            }
+
+            // Filter by description
+            if (!string.IsNullOrEmpty(DescriptionFilter))
+            {
+                filteredSymbols = filteredSymbols.Where(a => a.Description.Contains(DescriptionFilter)).ToList();
+            }
+
+            SymbolOptions = filteredSymbols;
+        }
+
+        private List<CFSymbol> _symbolOptions { get; set; }
+        public List<CFSymbol> SymbolOptions
+        {
+            get => _symbolOptions;
+            set
+            {
+                _symbolOptions = value;
+                NotifyPropertyChanged();
+            }
+        }
+
+        private CFSymbol _symbol;
+        public CFSymbol Symbol
+        {
+            get => _symbol;
+            set
+            {
+                SetProperty(ref _symbol, value, () => Symbol);
+                ValidateSymbol(Symbol, "Symbol");
+            }
+        }
+
+        private string _identityConfidence;
+        public string IdentityConfidence
+        {
+            get => _identityConfidence;
+            set
+            {
+                SetProperty(ref _identityConfidence, value, () => IdentityConfidence);
+                ValidateRequiredString(IdentityConfidence, "IdentityConfidence");
+            }
+        }
+
+        private string _existenceConfidence;
+        public string ExistenceConfidence
+        {
+            get => _existenceConfidence;
+            set
+            {
+                SetProperty(ref _existenceConfidence, value, () => ExistenceConfidence);
+                ValidateRequiredString(ExistenceConfidence, "ExistenceConfidence");
+            }
+        }
+
+        private string _locationConfidenceMeters;
+        public string LocationConfidenceMeters
+        {
+            get => _locationConfidenceMeters;
+            set
+            {
+                SetProperty(ref _locationConfidenceMeters, value, () => LocationConfidenceMeters);
+                ValidateRequiredString(LocationConfidenceMeters, "LocationConfidenceMeters");
+            }
+        }
+
+        private bool _isConcealed;
+        public bool IsConcealed
+        {
+            get => _isConcealed;
+            set => SetProperty(ref _isConcealed, value, () => IsConcealed);
+        }
+
+        // Convert bool to a Y/N char
+        public string IsConcealedString => IsConcealed ? "Y" : "N";
+
+        private string _notes;
+        public string Notes
+        {
+            get => _notes;
+            set => SetProperty(ref _notes, value, () => Notes);
+        }
+
+        private string _dataSource;
+        public string DataSource
+        {
+            get => _dataSource;
+            set => SetProperty(ref _dataSource, value, () => DataSource);
+        }
 
         #region ### Validation ####
 
@@ -66,25 +255,33 @@ namespace Geomapmaker.ViewModels.ContactsFaults
 
         public bool HasErrors => _validationErrors.Count > 0;
 
-        private void ValidateChangeWasMade()
+        // Validate symbol
+        private void ValidateSymbol(CFSymbol symbol, string propertyKey)
         {
-            // Error message isn't display on a field. Just prevents user from hitting update until a change is made.
-            const string propertyKey = "SilentError";
-
-            if (Selected == null)
+            // Required field
+            if (symbol == null)
+            {
+                _validationErrors[propertyKey] = new List<string>() { "" };
+            }
+            else
             {
                 _validationErrors.Remove(propertyKey);
-                return;
             }
 
-            //if (Selected.Name == Name && Selected.Description == Description)
-            //{
-            //    _validationErrors[propertyKey] = new List<string>() { "No changes have been made." };
-            //}
-            //else
-            //{
-            //    _validationErrors.Remove(propertyKey);
-            //}
+            RaiseErrorsChanged(propertyKey);
+        }
+
+        private void ValidateRequiredString(string text, string propertyKey)
+        {
+            // Required field
+            if (string.IsNullOrEmpty(text))
+            {
+                _validationErrors[propertyKey] = new List<string>() { "" };
+            }
+            else
+            {
+                _validationErrors.Remove(propertyKey);
+            }
 
             RaiseErrorsChanged(propertyKey);
         }
