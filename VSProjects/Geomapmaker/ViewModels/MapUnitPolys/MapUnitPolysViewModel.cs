9dea8e07b5f70f1b7937e88fd7254a9e47bf107e
Michael Camp <camp@email.arizona.edu>
2022-02-04 14:26:08 -0700

    Added a new prowindow for MapUnitPolys to the ribbon

---
+++ VSProjects/Geomapmaker/ViewModels/MapUnitPolys/MapUnitPolysViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace Geomapmaker.ViewModels.MapUnitPolys
+{
+    public class MapUnitPolysViewModel
+    {
+
+    }
+
+    internal class ShowMapUnitPolys : Button
+    {
+        private Views.MapUnitPolys.MapUnitPolys _mapunitpolys = null;
+
+        protected override void OnClick()
+        {
+            //already open?
+            if (_mapunitpolys != null)
+                return;
+            _mapunitpolys = new Views.MapUnitPolys.MapUnitPolys();
+            _mapunitpolys.Owner = FrameworkApplication.Current.MainWindow;
+            _mapunitpolys.Closed += (o, e) => { _mapunitpolys = null; };
+            _mapunitpolys.Show();
+            //uncomment for modal
+            //_mapunitpolys.ShowDialog();
+        }
+
+    }
+}
