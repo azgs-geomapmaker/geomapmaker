d5ae274204a54a12dd11fea8f0857429236d903a
Michael Camp <camp@email.arizona.edu>
2022-03-03 13:59:54 -0700

    Created a view and viewmodel for Stations

---
+++ VSProjects/Geomapmaker/ViewModels/Stations/Stations.cs
@@ -0,0 +1,25 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace Geomapmaker.ViewModels.Stations
+{
+    internal class ShowStations : Button
+    {
+
+        private Views.Stations.Stations _stations = null;
+
+        protected override void OnClick()
+        {
+            //already open?
+            if (_stations != null)
+                return;
+            _stations = new Views.Stations.Stations();
+            _stations.Owner = FrameworkApplication.Current.MainWindow;
+            _stations.Closed += (o, e) => { _stations = null; };
+            _stations.Show();
+            //uncomment for modal
+            //_stations.ShowDialog();
+        }
+
+    }
+}
