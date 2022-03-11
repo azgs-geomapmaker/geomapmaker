f02786287c30055995ef3dd9bb9416bcc2c0d8fb
Michael Camp <camp@email.arizona.edu>
2022-03-11 15:24:11 -0700

    Added a new view/viewmodel for Orientation Points

---
+++ VSProjects/Geomapmaker/ViewModels/OrientationPoints/OrientationPointsViewModel.cs
@@ -0,0 +1,41 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Controls;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Geomapmaker.ViewModels.OrientationPoints
+{
+    public class OrientationPointsViewModel
+    {
+    }
+
+    internal class ShowOrientationPoints : Button
+    {
+        private Views.OrientationPoints.OrientationPoints _orientationpoints = null;
+
+        protected override void OnClick()
+        {
+            //already open?
+            if (_orientationpoints != null)
+            {
+                _orientationpoints.Close();
+                return;
+            }
+
+            _orientationpoints = new Views.OrientationPoints.OrientationPoints
+            {
+                Owner = System.Windows.Application.Current.MainWindow
+            };
+
+            _orientationpoints.Closed += (o, e) => { _orientationpoints = null; };
+
+            //_orientationpoints.stationsVM.WindowCloseEvent += (s, e) => _stations.Close();
+
+            _orientationpoints.Show();
+        }
+    }
+
+}
