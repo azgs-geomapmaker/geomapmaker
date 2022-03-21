210d44b191f8d11b1b1826a9e549b705401c37a0
Michael Camp <camp@email.arizona.edu>
2022-03-21 10:10:24 -0700

    Update OP symbology after creating new OP

--- VSProjects/Geomapmaker/ViewModels/OrientationPoints/CreateOrientationPointVM.cs
+++ VSProjects/Geomapmaker/ViewModels/OrientationPoints/CreateOrientationPointVM.cs
@@ -109,6 +109,7 @@ namespace Geomapmaker.ViewModels.OrientationPoints
 
             if (IsSucceeded)
             {
+                Data.OrientationPoints.RebuildOrientationPointsSymbology();
                 ParentVM.CloseProwindow();
             }
             else
