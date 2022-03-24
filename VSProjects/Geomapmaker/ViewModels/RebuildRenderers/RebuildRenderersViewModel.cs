686f710378592be854ec9769b859faafd1b25f54
Michael Camp <camp@email.arizona.edu>
2022-03-24 08:59:32 -0700

    Added folders and viewmodel for RebuildRenderers

---
+++ VSProjects/Geomapmaker/ViewModels/RebuildRenderers/RebuildRenderersViewModel.cs
@@ -0,0 +1,29 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace Geomapmaker.ViewModels.RebuildRenderers
+{
+    public class RebuildRenderersViewModel
+    {
+    }
+
+    internal class ShowRebuildRenderers : Button
+    {
+
+        private Geomapmaker.RebuildRenderers _rebuildrenderers = null;
+
+        protected override void OnClick()
+        {
+            //already open?
+            if (_rebuildrenderers != null)
+                return;
+            _rebuildrenderers = new Geomapmaker.RebuildRenderers();
+            _rebuildrenderers.Owner = FrameworkApplication.Current.MainWindow;
+            _rebuildrenderers.Closed += (o, e) => { _rebuildrenderers = null; };
+            _rebuildrenderers.Show();
+            //uncomment for modal
+            //_rebuildrenderers.ShowDialog();
+        }
+
+    }
+}
