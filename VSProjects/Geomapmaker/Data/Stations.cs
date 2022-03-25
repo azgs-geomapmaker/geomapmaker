4e7099fe0f1197d623c32d021a1c84f63ea91931
Michael Camp <camp@email.arizona.edu>
2022-03-25 13:23:31 -0700

    Added a data method to get all Station rows

--- VSProjects/Geomapmaker/Data/Stations.cs
+++ VSProjects/Geomapmaker/Data/Stations.cs
@@ -1,5 +1,7 @@
 ﻿using ArcGIS.Core.Data;
 using ArcGIS.Desktop.Mapping;
+using Geomapmaker._helpers;
+using Geomapmaker.Models;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,6 +9,45 @@ namespace Geomapmaker.Data
 {
     public class Stations
     {
+        public static List<Station> GetStations()
+        {
+            List<Station> StationsList = new List<Station>();
+
+            FeatureLayer stationsLayer = MapView.Active?.Map.FindLayers("Stations").FirstOrDefault() as FeatureLayer;
+
+            if (stationsLayer == null)
+            {
+                return StationsList;
+            }
+
+            Table enterpriseTable = stationsLayer.GetTable();
+
+            using (RowCursor rowCursor = enterpriseTable.Search())
+            {
+                while (rowCursor.MoveNext())
+                {
+                    using (Row row = rowCursor.Current)
+                    {
+                        Station newStation = new Station
+                        {
+                            FieldID = Helpers.RowValueToString(row["FieldID"]),
+                            TimeDate = Helpers.RowValueToString(row["TimeDate"]),
+                            Observer = Helpers.RowValueToString(row["Observer"]),
+                            LocationMethod = Helpers.RowValueToString(row["LocationMethod"]),
+                            LocationConfidenceMeters = Helpers.RowValueToString(row["LocationConfidenceMeters"]),
+                            PlotAtScale = Helpers.RowValueToString(row["PlotAtScale"]),
+                            Notes = Helpers.RowValueToString(row["Notes"]),
+                            DataSourceId = Helpers.RowValueToString(row["DataSourceId"])
+                        };
+
+                        StationsList.Add(newStation);
+                    }
+                }
+            }
+
+            return StationsList;
+        }
+
         public static List<string> GetStationFieldIds()
         {
             List<string> FieldIds = new List<string>();
