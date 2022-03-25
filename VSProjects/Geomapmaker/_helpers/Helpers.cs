06e35349add9a8c7387be6d01184781cf987a16c
Michael Camp <camp@email.arizona.edu>
2022-03-25 13:11:15 -0700

    Added a helper method for converting row objects to string

---
+++ VSProjects/Geomapmaker/_helpers/Helpers.cs
@@ -0,0 +1,12 @@
+﻿namespace Geomapmaker._helpers
+{
+    public class Helpers
+    {
+        // Convert row object to a string. 
+        public static string RowValueToString(object value)
+        {
+            // Nulls are converted to an empty string
+            return value == null ? "" : value.ToString();
+        }
+    }
+}
