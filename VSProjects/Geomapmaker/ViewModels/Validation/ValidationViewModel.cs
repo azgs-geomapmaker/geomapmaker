7d19a514f31fc172122d0c2f2254cbd9b52d4475
Michael Camp <camp@email.arizona.edu>
2022-04-11 10:09:18 -0700

    Added prowindow for validation. Still setting up view and viewmodel

---
+++ VSProjects/Geomapmaker/ViewModels/Validation/ValidationViewModel.cs
@@ -0,0 +1,56 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using System;
+using System.Windows.Input;
+
+namespace Geomapmaker.ViewModels.Validation
+{
+    public class ValidationViewModel
+    {
+        public event EventHandler WindowCloseEvent;
+
+        public ICommand CommandCancel => new RelayCommand(() => CloseProwindow());
+
+        public ICommand CommandValidate => new RelayCommand(() => Validate());
+
+        public void Validate()
+        {
+
+
+
+
+
+        }
+
+        public void CloseProwindow()
+        {
+            WindowCloseEvent(this, new EventArgs());
+        }
+    }
+
+    internal class ShowValidation : Button
+    {
+
+        private Views.Validation.Validation _validation = null;
+
+        protected override void OnClick()
+        {
+            if (_validation != null)
+            {
+                _validation.Close();
+                return;
+            }
+
+            _validation = new Views.Validation.Validation
+            {
+                Owner = FrameworkApplication.Current.MainWindow
+            };
+
+            _validation.Closed += (o, e) => { _validation = null; };
+
+            _validation.validationVM.WindowCloseEvent += (s, e) => _validation.Close();
+
+            _validation.Show();
+        }
+    }
+}
---
+++ VSProjects/Geomapmaker/Views/Validation/Validation.xaml.cs
@@ -0,0 +1,32 @@
+﻿using Geomapmaker.ViewModels.Validation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Geomapmaker.Views.Validation
+{
+    /// <summary>
+    /// Interaction logic for Validation.xaml
+    /// </summary>
+    public partial class Validation : ArcGIS.Desktop.Framework.Controls.ProWindow
+    {
+        public ValidationViewModel validationVM = new ValidationViewModel();
+
+        public Validation()
+        {
+            InitializeComponent();
+            DataContext = validationVM;
+        }
+    }
+}
