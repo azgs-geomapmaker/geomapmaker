eceecbf9225a5bab4bda13db21c9ff5ac37ec1d1
Michael Camp <camp@email.arizona.edu>
2022-04-29 08:13:09 -0700

    Created a general data method for getting unique values for a specific field from any Feature Layer

---
+++ VSProjects/Geomapmaker/Data/General.cs
@@ -0,0 +1,54 @@
+﻿using ArcGIS.Core.Data;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using ArcGIS.Desktop.Mapping;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Geomapmaker.Data
+{
+    public class General
+    {
+        public static async Task<List<string>> GetUniqueValuesForFieldAsync(string fieldName, string tableName)
+        {
+            List<string> uniqueValues = new List<string>();
+
+            FeatureLayer layer = MapView.Active?.Map.FindLayers(tableName).FirstOrDefault() as FeatureLayer;
+
+            if (layer != null)
+            {
+                await QueuedTask.Run(() =>
+                {
+                    using (Table table = layer.GetTable())
+                    {
+                        QueryFilter queryFilter = new QueryFilter
+                        {
+                            SubFields = fieldName
+                        };
+
+                        using (RowCursor rowCursor = table.Search(queryFilter))
+                        {
+                            while (rowCursor.MoveNext())
+                            {
+                                using (Row row = rowCursor.Current)
+                                {
+                                    if (row[fieldName] != null)
+                                    {
+                                        string fieldValue = row[fieldName]?.ToString();
+
+                                        if (!string.IsNullOrEmpty(fieldValue) && !uniqueValues.Contains(fieldValue))
+                                        {
+                                            uniqueValues.Add(fieldValue);
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                });
+            }
+
+            return uniqueValues;
+        }
+    }
+}
