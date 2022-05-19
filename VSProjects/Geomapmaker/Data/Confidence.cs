b38f6f14c3bea90214520c942dd9e07f8cc01596
Michael Camp <camp@email.arizona.edu>
2022-05-19 13:53:35 -0700

    Removed -9 from LocationConfidenceMeters options after talking with Andrew

--- VSProjects/Geomapmaker/Data/Confidence.cs
+++ VSProjects/Geomapmaker/Data/Confidence.cs
@@ -11,18 +11,7 @@ namespace Geomapmaker.Data
             "Low"
         };
 
-        // The USGS GeMS template has Domain values set for the ContactsAndFaults LocationConfidenceMeters values.
-        // TODO: Remove the domain during database creation so we can use the -9 option
-        public static ObservableCollection<string> ContactsFaultsLocationConfidenceMeters => new ObservableCollection<string>() {
-            "10",
-            "25",
-            "50",
-            "100",
-            "250",
-        };
-
         public static ObservableCollection<string> LocationConfidenceMeters => new ObservableCollection<string>() {
-            "-9 (Unknown)",
             "10",
             "25",
             "50",
