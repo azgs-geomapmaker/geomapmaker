4d5051e114217c33e70009a554c90036577b5e18
Michael Camp <camp@email.arizona.edu>
2022-05-27 10:37:26 -0700

    Create MUP_Create folders and view model

---
+++ VSProjects/Geomapmaker/ViewModels/MapUnitPolysCreate/MapUnitPolysCreateVM.cs
@@ -0,0 +1,29 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace Geomapmaker.ViewModels.MapUnitPolysCreate
+{
+    public class MapUnitPolysCreateVM
+    {
+    }
+
+    internal class ShowMapUnitPolysCreate : Button
+    {
+
+        private Views.MapUnitPolysCreate.MapUnitPolysCreate _mapunitpolyscreate = null;
+
+        protected override void OnClick()
+        {
+            //already open?
+            if (_mapunitpolyscreate != null)
+                return;
+            _mapunitpolyscreate = new Views.MapUnitPolysCreate.MapUnitPolysCreate();
+            _mapunitpolyscreate.Owner = FrameworkApplication.Current.MainWindow;
+            _mapunitpolyscreate.Closed += (o, e) => { _mapunitpolyscreate = null; };
+            _mapunitpolyscreate.Show();
+            //uncomment for modal
+            //_mapunitpolyscreate.ShowDialog();
+        }
+
+    }
+}
