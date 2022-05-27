3df449b60b27df5ad8995504bb33826a7417a8d0
Michael Camp <camp@email.arizona.edu>
2022-05-27 10:13:18 -0700

    Added prowindow

---
+++ VSProjects/Geomapmaker/ViewModels/MapUnitPolysEdit/MapUnitPolysEditVM.cs
@@ -0,0 +1,28 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace Geomapmaker.ViewModels.MapUnitPolysEdit
+{
+    public class MapUnitPolysEditVM
+    {
+    }
+
+    internal class ShowMapUnitPolysEdit : Button
+    {
+        private Views.MapUnitPolysEdit.MapUnitPolysEdit _mapunitpolysedit = null;
+
+        protected override void OnClick()
+        {
+            //already open?
+            if (_mapunitpolysedit != null)
+                return;
+            _mapunitpolysedit = new Views.MapUnitPolysEdit.MapUnitPolysEdit();
+            _mapunitpolysedit.Owner = FrameworkApplication.Current.MainWindow;
+            _mapunitpolysedit.Closed += (o, e) => { _mapunitpolysedit = null; };
+            _mapunitpolysedit.Show();
+            //uncomment for modal
+            //_mapunitpolysedit.ShowDialog();
+        }
+
+    }
+}
---
+++ VSProjects/Geomapmaker/Views/MapUnitPolysEdit/MapUnitPolysEdit.xaml.cs
@@ -0,0 +1,18 @@
+﻿using Geomapmaker.ViewModels.MapUnitPolysEdit;
+
+namespace Geomapmaker.Views.MapUnitPolysEdit
+{
+    /// <summary>
+    /// Interaction logic for MapUnitPolysEdit.xaml
+    /// </summary>
+    public partial class MapUnitPolysEdit : ArcGIS.Desktop.Framework.Controls.ProWindow
+    {
+        public MapUnitPolysEditVM mapUnitPolysEditVM = new MapUnitPolysEditVM();
+
+        public MapUnitPolysEdit()
+        {
+            InitializeComponent();
+            DataContext = mapUnitPolysEditVM;
+        }
+    }
+}
