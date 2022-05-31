2df96436af6cbf44adb3cfd3b8c4f5229ddbc327
Michael Camp <camp@email.arizona.edu>
2022-05-31 12:37:19 -0700

    Finished hooking up the datacontext for the view.

--- VSProjects/Geomapmaker/ViewModels/Export/ExportVM.cs
+++ VSProjects/Geomapmaker/ViewModels/Export/ExportVM.cs
@@ -1,29 +1,106 @@
 ﻿using ArcGIS.Desktop.Framework;
 using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
 
 namespace Geomapmaker.ViewModels.Export
 {
-    public class ExportVM
+    public class ExportVM : ProWindow, INotifyPropertyChanged, INotifyDataErrorInfo
     {
+        public event EventHandler WindowCloseEvent;
+
+        public ICommand CommandCancel => new RelayCommand(() => CloseProwindow());
+
+        public ExportVM()
+        {
+
+        }
+
+        public void CloseProwindow()
+        {
+            WindowCloseEvent(this, new EventArgs());
+        }
+
+        #region Validation
+
+        private readonly Dictionary<string, ICollection<string>> _validationErrors = new Dictionary<string, ICollection<string>>();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        private void RaiseErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        public System.Collections.IEnumerable GetErrors(string propertyName)
+        {
+            // Return null if parameters is null/empty OR there are no errors for that parameter
+            // Otherwise, return the errors for that parameter.
+            return string.IsNullOrEmpty(propertyName) || !_validationErrors.ContainsKey(propertyName) ?
+                null : (System.Collections.IEnumerable)_validationErrors[propertyName];
+        }
+
+        public bool HasErrors => _validationErrors.Count > 0;
+
+        private void ValidateRequiredString(string text, string propertyKey)
+        {
+            // Required field
+            if (string.IsNullOrEmpty(text))
+            {
+                _validationErrors[propertyKey] = new List<string>() { "" };
+            }
+            else
+            {
+                _validationErrors.Remove(propertyKey);
+            }
+
+            RaiseErrorsChanged(propertyKey);
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
     }
 
     internal class ShowExport : Button
     {
-
         private Views.Export.Export _export = null;
 
         protected override void OnClick()
         {
             //already open?
             if (_export != null)
+            {
+                _export.Close();
                 return;
-            _export = new Views.Export.Export();
-            _export.Owner = FrameworkApplication.Current.MainWindow;
-            _export.Closed += (o, e) => { _export = null; };
-            _export.Show();
-            //uncomment for modal
-            //_export.ShowDialog();
-        }
+            }
 
+            _export = new Views.Export.Export
+            {
+                Owner = System.Windows.Application.Current.MainWindow
+            };
+
+            _export.Closed += (o, e) =>
+            {
+                _export = null;
+            };
+
+            _export.exportVM.WindowCloseEvent += (s, e) => _export.Close();
+
+            _export.Show();
+        }
     }
 }
--- VSProjects/Geomapmaker/Views/Export/Export.xaml.cs
+++ VSProjects/Geomapmaker/Views/Export/Export.xaml.cs
@@ -1,17 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using Geomapmaker.ViewModels.Export;
 
 namespace Geomapmaker.Views.Export
 {
@@ -20,9 +7,12 @@ namespace Geomapmaker.Views.Export
     /// </summary>
     public partial class Export : ArcGIS.Desktop.Framework.Controls.ProWindow
     {
+        public ExportVM exportVM = new ExportVM();
+
         public Export()
         {
             InitializeComponent();
+            DataContext = exportVM;
         }
     }
 }
