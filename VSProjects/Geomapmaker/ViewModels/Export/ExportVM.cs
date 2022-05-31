a8045460aa2acd9c8704f69a784e405db1e6dbb0
Michael Camp <camp@email.arizona.edu>
2022-05-31 12:21:14 -0700

    Created 'Export' group in the ribbon. Switched up some button colors

---
+++ VSProjects/Geomapmaker/ViewModels/Export/ExportVM.cs
@@ -0,0 +1,29 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+
+namespace Geomapmaker.ViewModels.Export
+{
+    public class ExportVM
+    {
+    }
+
+    internal class ShowExport : Button
+    {
+
+        private Views.Export.Export _export = null;
+
+        protected override void OnClick()
+        {
+            //already open?
+            if (_export != null)
+                return;
+            _export = new Views.Export.Export();
+            _export.Owner = FrameworkApplication.Current.MainWindow;
+            _export.Closed += (o, e) => { _export = null; };
+            _export.Show();
+            //uncomment for modal
+            //_export.ShowDialog();
+        }
+
+    }
+}
