430cf6e2dfbd6e2e014601be8796bf889c9d3043
Michael Camp <camp@email.arizona.edu>
2022-06-02 11:13:53 -0700

    Added a test button to the ribbon for creating a report

---
+++ VSProjects/Geomapmaker/Report.cs
@@ -0,0 +1,13 @@
+﻿using ArcGIS.Desktop.Framework.Contracts;
+
+namespace Geomapmaker
+{
+    internal class Report : Button
+    {
+        protected override void OnClick()
+        {
+
+
+        }
+    }
+}
