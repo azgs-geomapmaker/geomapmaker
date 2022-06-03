5dac86497b0a19290e810f03196257ccce1db518
Michael Camp <camp@email.arizona.edu>
2022-06-03 09:31:34 -0700

    Implemented an standalone toolbar validation check that can be used in both the validation window and the report.

---
+++ VSProjects/Geomapmaker/Data/GeomapmakerValidation.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Geomapmaker.Data
+{
+    public class GeomapmakerValidation
+    {
+        public static async Task<Dictionary<string, List<string>>> GetErrorsAsync()
+        {
+            Dictionary<string, List<string>> _validationErrors = new Dictionary<string, List<string>>
+            {
+                { "Symbology", await GetSymbologyErrorsAsync() },
+                { "DataSources", new List<string>() },
+                { "DescriptionOfMapUnits", await GetDescriptionOfMapUnitsErrorsAsync() },
+                { "Glossary", new List<string>() },
+                { "GeoMaterialDict", new List<string>() },
+                { "MapUnitPolys", new List<string>() },
+                { "ContactsAndFaults", new List<string>() },
+                { "Stations", new List<string>() },
+                { "OrientationPoints", new List<string>() }
+            };
+
+            return _validationErrors;
+        }
+
+        private static async Task<List<string>> GetSymbologyErrorsAsync()
+        {
+            List<string> errors = new List<string>();
+
+            // Check if the table exists
+            if (await General.StandaloneTableExistsAsync("Symbology") == false)
+            {
+                errors.Add("Table not found: Symbology");
+            }
+            else // Table was found
+            {
+                //
+                // Check for duplicate tables
+                //
+                int tableCount = General.StandaloneTableCount("Symbology");
+                if (tableCount > 1)
+                {
+                    errors.Add($"Multiple Symbology tables found");
+                }
+
+                //
+                // Check for any missing fields
+                //
+
+                // List of required fields
+                List<string> symbologyRequiredFields = new List<string>() { "type", "key_", "description", "symbol" };
+
+                // Get missing fields
+                List<string> missingFields = await General.StandaloneTableGetMissingFieldsAsync("Symbology", symbologyRequiredFields);
+                // Add errors for any missing fields
+                foreach (string field in missingFields)
+                {
+                    errors.Add($"Field not found: {field}");
+                }
+
+                //
+                // Check for any missing CF symbols
+                //
+                List<string> missingCFsymbols = await Symbology.GetMissingContactsAndFaultsSymbologyAsync();
+                foreach (string key in missingCFsymbols)
+                {
+                    errors.Add($"Missing line symbology: {key}");
+                }
+
+                //
+                // Check for any missing OP symbols
+                //
+                List<string> missingOPsymbols = await Symbology.GetMissingOrientationPointsSymbologyAsync();
+                foreach (string key in missingOPsymbols)
+                {
+                    errors.Add($"Missing point symbology: {key}");
+                }
+            }
+
+            return errors;
+        }
+
+        private static async Task<List<string>> GetDescriptionOfMapUnitsErrorsAsync()
+        {
+            List<string> errors = new List<string>();
+
+            // Check if the table exists
+            if (await General.StandaloneTableExistsAsync("DescriptionOfMapUnits"))
+            {
+                //
+                // Check table for missing toolbar fields
+                //
+
+                // List of required fields to check
+                List<string> dmuRequiredFields = new List<string>() { "relativeage", "hexcolor" };
+
+                // Get misssing required fields
+                List<string> missingFields = await General.StandaloneTableGetMissingFieldsAsync("DescriptionOfMapUnits", dmuRequiredFields);
+                foreach (string field in missingFields)
+                {
+                    errors.Add($"Field not found: {field}");
+                }
+            }
+
+            return errors;
+        }
+    }
+}
--- VSProjects/Geomapmaker/ViewModels/Validation/GeomapmakerVM.cs
+++ VSProjects/Geomapmaker/ViewModels/Validation/GeomapmakerVM.cs
@@ -19,337 +19,67 @@ namespace Geomapmaker.ViewModels.Validation
 
         public async Task Validate()
         {
-            Result1 = await Check1Async("Result1");
-            NotifyPropertyChanged("Result1");
+            Dictionary<string, List<string>> errors = await GeomapmakerValidation.GetErrorsAsync();
 
-            Result2 = await Check2Async("Result2");
-            NotifyPropertyChanged("Result2");
+            Symbology = errors["Symbology"]?.Count == 0 ? "Passed" : "Failed";
+            NotifyPropertyChanged("Symbology");
 
-            Result3 = await Check3Async("Result3");
-            NotifyPropertyChanged("Result3");
+            DataSources = errors["DataSources"]?.Count == 0 ? "Passed" : "Failed";
+            NotifyPropertyChanged("DataSources");
 
-            Result4 = await Check4Async("Result4");
-            NotifyPropertyChanged("Result4");
+            DescriptionOfMapUnits = errors["DescriptionOfMapUnits"]?.Count == 0 ? "Passed" : "Failed";
+            NotifyPropertyChanged("DescriptionOfMapUnits");
 
-            Result5 = await Check5Async("Result5");
-            NotifyPropertyChanged("Result5");
+            Glossary = errors["Glossary"]?.Count == 0 ? "Passed" : "Failed";
+            NotifyPropertyChanged("Glossary");
 
-            Result6 = await Check6Async("Result6");
-            NotifyPropertyChanged("Result6");
+            GeoMaterialDict = errors["GeoMaterialDict"]?.Count == 0 ? "Passed" : "Failed";
+            NotifyPropertyChanged("GeoMaterialDict");
 
-            Result7 = await Check7Async("Result7");
-            NotifyPropertyChanged("Result7");
+            MapUnitPolys = errors["MapUnitPolys"]?.Count == 0 ? "Passed" : "Failed";
+            NotifyPropertyChanged("MapUnitPolys");
 
-            Result8 = await Check8Async("Result8");
-            NotifyPropertyChanged("Result8");
+            ContactsAndFaults = errors["ContactsAndFaults"]?.Count == 0 ? "Passed" : "Failed";
+            NotifyPropertyChanged("ContactsAndFaults");
 
-            Result9 = await Check9Async("Result9");
-            NotifyPropertyChanged("Result9");
+            Stations = errors["Stations"]?.Count == 0 ? "Passed" : "Failed";
+            NotifyPropertyChanged("Stations");
+
+            OrientationPoints = errors["OrientationPoints"]?.Count == 0 ? "Passed" : "Failed";
+            NotifyPropertyChanged("OrientationPoints");
+
+            foreach (string key in errors.Keys)
+            {
+                if (errors[key]?.Count != 0)
+                {
+                    _validationErrors[key] = _helpers.Helpers.ErrorListToTooltip(errors[key]);
+                }
+            }
 
             ParentVM.UpdateGeomapmakerResults(_validationErrors.Count);
         }
 
-        public string Result1 { get; set; } = "Checking..";
-        public string Result2 { get; set; } = "Checking..";
-        public string Result3 { get; set; } = "Checking..";
-        public string Result4 { get; set; } = "Checking..";
-        public string Result5 { get; set; } = "Checking..";
-        public string Result6 { get; set; } = "Checking..";
-        public string Result7 { get; set; } = "Checking..";
-        public string Result8 { get; set; } = "Checking..";
-        public string Result9 { get; set; } = "Checking..";
+        // Tooltips
+        public string SymbologyTooltip => "Table exists.<br>No duplicate tables.<br>No missing fields.<br>";
+        public string DataSourcesTooltip => "";
+        public string DescriptionOfMapUnitsTooltip => "Check for toolbar fields.";
+        public string GlossaryTooltip => "";
+        public string GeoMaterialDictTooltip => "";
+        public string Tooltip => "";
+        public string MapUnitPolysTooltip => "";
+        public string ContactsAndFaultsTooltip => "";
+        public string StationsTooltip => "";
+        public string OrientationPointsTooltip => "";
 
-        // Symbology
-        public string Check1Tooltip => "Table exists.<br>" +
-                                       "No duplicate tables.<br>" +
-                                       "No missing fields.<br>";
-
-        // Symbology
-        private async Task<string> Check1Async(string propertyKey)
-        {
-            List<string> errors = new List<string>();
-
-            // Check if the table exists
-            if (await General.StandaloneTableExistsAsync("Symbology") == false)
-            {
-                errors.Add("Table not found: Symbology");
-            }
-            else // Table was found
-            {
-                //
-                // Check for duplicate tables
-                //
-                int tableCount = General.StandaloneTableCount("Symbology");
-                if (tableCount > 1)
-                {
-                    errors.Add($"Multiple Symbology tables found");
-                }
-
-                //
-                // Check for any missing fields
-                //
-
-                // List of required fields
-                List<string> symbologyRequiredFields = new List<string>() { "type", "key_", "description", "symbol" };
-
-                // Get missing fields
-                List<string> missingFields = await General.StandaloneTableGetMissingFieldsAsync("Symbology", symbologyRequiredFields);
-                // Add errors for any missing fields
-                foreach (string field in missingFields)
-                {
-                    errors.Add($"Field not found: {field}");
-                }
-
-                //
-                // Check for any missing CF symbols
-                //
-                List<string> missingCFsymbols = await Symbology.GetMissingContactsAndFaultsSymbologyAsync();
-                foreach (string key in missingCFsymbols)
-                {
-                    errors.Add($"Missing line symbology: {key}");
-                }
-
-                //
-                // Check for any missing OP symbols
-                //
-                List<string> missingOPsymbols = await Symbology.GetMissingOrientationPointsSymbologyAsync();
-                foreach (string key in missingOPsymbols)
-                {
-                    errors.Add($"Missing point symbology: {key}");
-                }
-            }
-
-            if (errors.Count == 0)
-            {
-                return "Passed";
-            }
-            else
-            {
-                _validationErrors[propertyKey] = _helpers.Helpers.ErrorListToTooltip(errors);
-                RaiseErrorsChanged(propertyKey);
-                return "Failed";
-            }
-        }
-
-        // DataSources
-        public string Check2Tooltip => "";
-
-        // DataSources
-        private async Task<string> Check2Async(string propertyKey)
-        {
-            List<string> errors = new List<string>();
-
-            // Check if the table exists
-            if (await General.StandaloneTableExistsAsync("DescriptionOfMapUnits"))
-            {
-                // Validation
-            }
-
-            if (errors.Count == 0)
-            {
-                return "Passed";
-            }
-            else
-            {
-                _validationErrors[propertyKey] = _helpers.Helpers.ErrorListToTooltip(errors);
-                RaiseErrorsChanged(propertyKey);
-                return "Failed";
-            }
-        }
-
-        // DescriptionOfMapUnits
-        public string Check3Tooltip => "Check for toolbar fields.";
-
-        // DescriptionOfMapUnits
-        private async Task<string> Check3Async(string propertyKey)
-        {
-            List<string> errors = new List<string>();
-
-            // Check if the table exists
-            if (await General.StandaloneTableExistsAsync("DescriptionOfMapUnits"))
-            {
-                //
-                // Check table for missing toolbar fields
-                //
-
-                // List of required fields to check
-                List<string> dmuRequiredFields = new List<string>() { "relativeage", "hexcolor" };
-
-                // Get misssing required fields
-                List<string> missingFields = await General.StandaloneTableGetMissingFieldsAsync("DescriptionOfMapUnits", dmuRequiredFields);
-                foreach (string field in missingFields)
-                {
-                    errors.Add($"Field not found: {field}");
-                }
-            }
-
-            if (errors.Count == 0)
-            {
-                return "Passed";
-            }
-            else
-            {
-                _validationErrors[propertyKey] = _helpers.Helpers.ErrorListToTooltip(errors);
-                RaiseErrorsChanged(propertyKey);
-                return "Failed";
-            }
-        }
-
-        // Glossary
-        public string Check4Tooltip => "";
-
-        // Glossary
-        private async Task<string> Check4Async(string propertyKey)
-        {
-            List<string> errors = new List<string>();
-
-            // Check if the table exists
-            if (await General.StandaloneTableExistsAsync("Glossary"))
-            {
-                // Validation
-            }
-
-            if (errors.Count == 0)
-            {
-                return "Passed";
-            }
-            else
-            {
-                _validationErrors[propertyKey] = _helpers.Helpers.ErrorListToTooltip(errors);
-                RaiseErrorsChanged(propertyKey);
-                return "Failed";
-            }
-        }
-
-        // GeoMaterialDict
-        public string Check5Tooltip => "";
-
-        // GeoMaterialDict
-        private async Task<string> Check5Async(string propertyKey)
-        {
-            List<string> errors = new List<string>();
-
-            // Check if the table exists
-            if (await General.StandaloneTableExistsAsync("GeoMaterialDict"))
-            {
-                // Validation
-            }
-
-            if (errors.Count == 0)
-            {
-                return "Passed";
-            }
-            else
-            {
-                _validationErrors[propertyKey] = _helpers.Helpers.ErrorListToTooltip(errors);
-                RaiseErrorsChanged(propertyKey);
-                return "Failed";
-            }
-        }
-
-        // MapUnitPolys
-        public string Check6Tooltip => "";
-
-        // MapUnitPolys
-        private async Task<string> Check6Async(string propertyKey)
-        {
-            List<string> errors = new List<string>();
-
-            // Check if the table exists
-            if (await General.FeatureLayerExistsAsync("MapUnitPolys"))
-            {
-                // Validation
-            }
-
-            if (errors.Count == 0)
-            {
-                return "Passed";
-            }
-            else
-            {
-                _validationErrors[propertyKey] = _helpers.Helpers.ErrorListToTooltip(errors);
-                RaiseErrorsChanged(propertyKey);
-                return "Failed";
-            }
-        }
-
-        // ContactsAndFaults
-        public string Check7Tooltip => "";
-
-        // ContactsAndFaults
-        private async Task<string> Check7Async(string propertyKey)
-        {
-            List<string> errors = new List<string>();
-
-            // Check if the table exists
-            if (await General.FeatureLayerExistsAsync("ContactsAndFaults"))
-            {
-                // Validation
-            }
-
-            if (errors.Count == 0)
-            {
-                return "Passed";
-            }
-            else
-            {
-                _validationErrors[propertyKey] = _helpers.Helpers.ErrorListToTooltip(errors);
-                RaiseErrorsChanged(propertyKey);
-                return "Failed";
-            }
-        }
-
-        // Stations
-        public string Check8Tooltip => "";
-
-        // Stations
-        private async Task<string> Check8Async(string propertyKey)
-        {
-            List<string> errors = new List<string>();
-
-            // Check if the table exists
-            if (await General.FeatureLayerExistsAsync("Stations"))
-            {
-                // Validation
-            }
-
-            if (errors.Count == 0)
-            {
-                return "Passed";
-            }
-            else
-            {
-                _validationErrors[propertyKey] = _helpers.Helpers.ErrorListToTooltip(errors);
-                RaiseErrorsChanged(propertyKey);
-                return "Failed";
-            }
-        }
-
-        // OrientationPoints
-        public string Check9Tooltip => "";
-
-        // OrientationPoints
-        private async Task<string> Check9Async(string propertyKey)
-        {
-            List<string> errors = new List<string>();
-
-            // Check if the table exists
-            if (await General.FeatureLayerExistsAsync("OrientationPoints"))
-            {
-                // Validation
-            }
-
-            if (errors.Count == 0)
-            {
-                return "Passed";
-            }
-            else
-            {
-                _validationErrors[propertyKey] = _helpers.Helpers.ErrorListToTooltip(errors);
-                RaiseErrorsChanged(propertyKey);
-                return "Failed";
-            }
-        }
+        public string Symbology { get; set; } = "Checking..";
+        public string DataSources { get; set; } = "Checking..";
+        public string DescriptionOfMapUnits { get; set; } = "Checking..";
+        public string Glossary { get; set; } = "Checking..";
+        public string GeoMaterialDict { get; set; } = "Checking..";
+        public string MapUnitPolys { get; set; } = "Checking..";
+        public string ContactsAndFaults { get; set; } = "Checking..";
+        public string Stations { get; set; } = "Checking..";
+        public string OrientationPoints { get; set; } = "Checking..";
 
         #region Validation
 
