937a985f367e9e8ca1fc726d6eaa9d9871e7d59d
Michael Camp <camp@email.arizona.edu>
2022-06-08 09:10:26 -0700

    Created validationrule model

---
+++ VSProjects/Geomapmaker/Models/ValidationRule.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace Geomapmaker.Models
+{
+    public class ValidationRule
+    {
+        public string Description { get; set; }
+
+        public ValidationStatus Status { get; set; } = ValidationStatus.Skipped;
+
+        public List<string> Errors { get; set; } = new List<string>();
+    }
+
+    public enum ValidationStatus
+    {
+        Skipped,
+        Passed,
+        Failed
+    }
+}
