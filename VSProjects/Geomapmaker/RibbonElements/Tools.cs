e2912ad21675dedd1c6d82d454dd54c97c2ed440
Michael Camp <camp@email.arizona.edu>
2022-06-09 16:24:23 -0700

    Added a menu item to the ribbon

---
+++ VSProjects/Geomapmaker/RibbonElements/Tools.cs
@@ -0,0 +1,26 @@
+﻿using ArcGIS.Desktop.Framework.Contracts;
+
+namespace Geomapmaker.RibbonElements
+{
+    internal class Tools_button1 : Button
+    {
+        protected override void OnClick()
+        {
+        }
+    }
+
+    internal class Tools_button2 : Button
+    {
+        protected override void OnClick()
+        {
+        }
+    }
+
+    internal class Tools_button3 : Button
+    {
+        protected override void OnClick()
+        {
+        }
+    }
+
+}
