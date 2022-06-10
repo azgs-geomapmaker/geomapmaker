c649b42431424b00b03882c1d047adbc1687ffe8
Michael Camp <camp@email.arizona.edu>
2022-06-10 09:17:01 -0700

    Created glossary view/viewmodel

---
+++ VSProjects/Geomapmaker/ViewModels/Glossary/GlossaryVM.cs
@@ -0,0 +1,102 @@
+﻿using ArcGIS.Desktop.Framework;
+using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Controls;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows.Input;
+
+namespace Geomapmaker.ViewModels.Glossary
+{
+    public class GlossaryVM : ProWindow, INotifyPropertyChanged, INotifyDataErrorInfo
+    {
+        public event EventHandler WindowCloseEvent;
+
+        public ICommand CommandCancel => new RelayCommand(() => CloseProwindow());
+
+        public void CloseProwindow()
+        {
+            WindowCloseEvent(this, new EventArgs());
+        }
+
+        #region Validation
+
+        private readonly Dictionary<string, ICollection<string>> _validationErrors = new Dictionary<string, ICollection<string>>();
+
+        public event EventHandler<DataErrorsChangedEventArgs> ErrorsChanged;
+
+        private void RaiseErrorsChanged(string propertyName)
+        {
+            ErrorsChanged?.Invoke(this, new DataErrorsChangedEventArgs(propertyName));
+        }
+
+        public System.Collections.IEnumerable GetErrors(string propertyName)
+        {
+            // Return null if parameters is null/empty OR there are no errors for that parameter
+            // Otherwise, return the errors for that parameter.
+            return string.IsNullOrEmpty(propertyName) || !_validationErrors.ContainsKey(propertyName) ?
+                null : (System.Collections.IEnumerable)_validationErrors[propertyName];
+        }
+
+        public bool HasErrors => _validationErrors.Count > 0;
+
+        private void ValidateRequiredString(string text, string propertyKey)
+        {
+            // Required field
+            if (string.IsNullOrEmpty(text))
+            {
+                _validationErrors[propertyKey] = new List<string>() { "" };
+            }
+            else
+            {
+                _validationErrors.Remove(propertyKey);
+            }
+
+            RaiseErrorsChanged(propertyKey);
+        }
+
+        #endregion
+
+        #region INotifyPropertyChanged
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] string propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        #endregion
+    }
+
+    internal class ShowGlossary : Button
+    {
+        private Views.Glossary.Glossary _glossary = null;
+
+        protected override void OnClick()
+        {
+            //already open?
+            if (_glossary != null)
+            {
+                _glossary.Close();
+                return;
+            }
+
+            _glossary = new Views.Glossary.Glossary
+            {
+                Owner = System.Windows.Application.Current.MainWindow
+            };
+
+            _glossary.Closed += (o, e) =>
+            {
+                _glossary = null;
+            };
+
+            _glossary.glossaryVM.WindowCloseEvent += (s, e) => _glossary.Close();
+
+            _glossary.Show();
+        }
+
+    }
+}
