f74fdcbb1526e4631857d47781b11113ad531626
Michael Camp <camp@email.arizona.edu>
2022-06-27 14:50:16 -0700

    Comment

--- VSProjects/Geomapmaker/Data/PredefinedTerms.cs
+++ VSProjects/Geomapmaker/Data/PredefinedTerms.cs
@@ -5,6 +5,13 @@ namespace Geomapmaker.Data
 {
     public class PredefinedTerms
     {
+        /// <summary>
+        /// Get predefined terms from the standalone table.
+        /// </summary>
+        /// <param name="datasetName"></param>
+        /// <param name="fieldName"></param>
+        /// <param name="term"></param>
+        /// <returns>GlossarTerm with the predefined definition or an empty string if it is not found.</returns>
         public static async Task<GlossaryTerm> GetPrepopulatedDefinitionAsync(string datasetName, string fieldName, string term)
         {
             string definition = await AnyStandaloneTable.GetValueFromWhereClauseAsync("PredefinedTerms", $"DatasetName = '{datasetName}' AND FieldName = '{fieldName}' AND Term = '{term}'", "Definition");
