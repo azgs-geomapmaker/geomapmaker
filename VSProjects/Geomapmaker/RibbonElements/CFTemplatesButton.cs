61c9f79b3cc6c5c1ecd8ea8112280fdbbc7bdb81
NoisyFlowers <noisyflowers@douglasmeredith.net>
2025-10-20 16:50:33 -0700

    New class for Button to trigger template generation

---
+++ VSProjects/Geomapmaker/RibbonElements/CFTemplatesButton.cs
@@ -0,0 +1,26 @@
+﻿using ArcGIS.Desktop.Framework.Contracts;
+using ArcGIS.Desktop.Framework.Threading.Tasks;
+using Geomapmaker.Data;
+using Geomapmaker.Report;
+using System.Diagnostics;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace Geomapmaker.RibbonElements
+{
+    internal class CFTemplatesButton : Button
+    {
+        protected override async void OnClick()
+        {
+            ProgressDialog progDialog = new ProgressDialog("Building Templates");
+
+            progDialog.Show();
+
+            //await Task.Run(() => ContactsAndFaults.RebuildContactsFaultsSymbology());
+            await ContactsAndFaults.GenerateTemplatesAsync();
+
+            progDialog.Hide();
+            
+        }
+    }
+}
